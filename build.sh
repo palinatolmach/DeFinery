350bbf9c9aff03241a610769816727c9693d8326
Palina Tolmach <polina.tolmach@gmail.com>
2022-08-04 23:04:37 +0800

    Minor update in build.sh

--- build.sh
+++ build.sh
@@ -6,18 +6,13 @@ git clone https://github.com/Microsoft/vcpkg.git &&\
     ./vcpkg integrate install &&\
     ./vcpkg install jsoncpp
 
-cd ..
-
- mkdir -p temp && cd /temp &&\
-   git clone https://github.com/Z3Prover/z3.git &&\
+git clone https://github.com/Z3Prover/z3.git &&\
    cd z3 && \
    git checkout z3-4.8.13 &&\
    python scripts/mk_make.py &&\
    cd build &&\
    make &&\
-   make install
-
-cd ../../..
+   make install && cd ../..
 
 unzip definery-see.zip
 chmod +x ./definery-see
@@ -31,17 +26,17 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" &&\
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" &&\
     cd SCRepair/sm &&\
-    npm install && npm run-script build
+    npm install && npm run-script build && cd ../
 
 # curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py &&\
 #     python3.7 ./get-pip.py &&\
 #     rm get-pip.py
 
-cd .. && python3.7 setup.py install
+python3.7 setup.py install
 
 pip install attrs charset_normalizer logbook deap docker
 
 mkdir contracts && mkdir experiments
-cd ..
-cp -r contracts/eval/* SCRepair/contracts/
-cp definery-see SCRepair/
+
+cp -r ../contracts/eval/* ./contracts/
+cp ../definery-see ./
